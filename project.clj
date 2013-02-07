66697ad7a88b7a2e887e82200fac1b8adaa562e4
Soren Macbeth <soren@dopeness.org>
2013-02-07 12:16:14 -0800

    add lein skeleton

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject aljebra "0.1.0-SNAPSHOT"
+  :description "A Clojure wrapper of twitter/algebird"
+  :url "https://github.com/sorenmacbeth/aljebra"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ test/aljebra/core_test.clj
@@ -0,0 +1,7 @@
+(ns aljebra.core-test
+  (:use clojure.test
+        aljebra.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
