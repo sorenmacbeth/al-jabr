f0f023fb5f51ec1a234b18641674cd0d3d4e2cfc
Soren Macbeth <soren@dopeness.org>
2013-02-07 14:20:27 -0800

    change name and deps

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject aljebra "0.1.0-SNAPSHOT"
+(defproject al-jebr "0.1.0-SNAPSHOT"
   :description "A Clojure wrapper of twitter/algebird"
-  :url "https://github.com/sorenmacbeth/aljebra"
+  :url "https://github.com/sorenmacbeth/al-jebr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :min-lein-version "2.0.0"
+  :source-paths ["src/clj"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.twitter/algebird-core_2.9.2 "0.1.8"]])
