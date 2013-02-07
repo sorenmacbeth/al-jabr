012b0085f1e99353964429aeba140e7d45027ee1
Soren Macbeth <soren@dopeness.org>
2013-02-07 14:23:41 -0800

    rename classes and tests

---
+++ src/clj/al_jebr/core.clj
@@ -0,0 +1,6 @@
+(ns al-jebr.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/al_jebr/core_test.clj
@@ -0,0 +1,7 @@
+(ns al-jebr.core-test
+  (:use clojure.test
+        al-jebr.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
