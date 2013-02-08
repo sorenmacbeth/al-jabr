ecc62de193a621c403ca7e502b839ce1bc6e2300
Sam Ritchie <sritchie@twitter.com>
2013-02-08 13:01:26 -0800

    add semigroups

--- src/clj/al_jabr/core.clj
+++ src/clj/al_jabr/core.clj
@@ -1,6 +1,30 @@
 (ns al-jabr.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn type-sym [algebird-type]
+  (symbol (str algebird-type "$/MODULE$")))
+
+(defn class-sym [algebird-type]
+  (symbol (str "com.twitter.algebird." algebird-type "$")))
+
+(defprotocol Semigroup
+  (plus [l r]))
+
+(defmacro defsemigroups
+  [m]
+  `(do ~@(for [r (map class-sym (vals m))]
+           `(import ~(symbol r)))
+       (extend-protocol Semigroup
+         ~@(mapcat (fn [[clojure-type algebird-type]]
+                     `[~clojure-type
+                       (~'plus [l# r#] (.plus ~(type-sym algebird-type) l# r#))])
+             m))))
+
+(defsemigroups
+  {nil NullGroup
+   Boolean JBoolField
+   Integer JIntRing
+   Short JShortRing
+   Long JLongRing
+   Float JFloatField
+   Double JDoubleField
+   String StringMonoid})
